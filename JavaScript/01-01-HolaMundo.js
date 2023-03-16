0cc6968dc437ffeac566f660890c8ec2abb9adc1
Pablo D <Pdiaz9731@gmail.com>
2023-03-16 08:38:16 -0600

    Javascript

---
+++ JavaScript/01-01-HolaMundo.js
@@ -0,0 +1,3 @@
+var nombre = " Pablo";
+console.log(nombre);
+
---
+++ JavaScript/02-01-Tiposdedatos.js
@@ -0,0 +1,58 @@
+//Tipo de dato String
+var nombre=" Pablo";
+console.log (nombre);
+
+//Tipo de dato Numerico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato Object
+var Object = { 
+    nombre : "Pablo",
+    Apellido : "Díaz",
+    Telefono : "987393983"
+}
+console.log(Object);
+
+
+//Tipo de dato boolean(True,False)
+var bandera = true;
+console.log(bandera);
+
+//Tipo de dato function
+function mifuncion(){}
+console.log(typeof mifuncion);
+
+//Tipo de dato Symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(typeof simbolo);
+
+//Tipo clase es una function
+class persona{
+    constructor(nombre,apellido){
+      this.nombre=nombre;
+      this.apellido=apellido;
+    }
+}
+console.log(typeof persona);
+
+//Tipo Undefined
+var x;
+console.log(x);
+
+//null= Ausencia de valos
+var y =null;
+console.log(y);
+
+//Preguntar que tipo de variable es 
+console.log(typeof nombre);
+
+//Array
+var autos = ["BMW","Audi","Volvo"];
+console.log(autos);
+console.log(typeof autos);
+
+//Dato Vacio
+var z ="";
+console.log(z)
+
